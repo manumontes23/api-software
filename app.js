41c3bf61f231a5dcd5ab94b6ea50994ff76d48dd
DaaGeney <46972313+DaaGeney@users.noreply.github.com>
2020-10-15 22:07:38 -0500

    correciones menores

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const setUpExpress = require("./config/express.config");
 
 setUpExpress(app);
 setUpRoutes(app);
+app.use((req, res, next) => {
+    res.status(404).send({
+      message: "Esta ruta no existe"
+    });
+  });
 
 
 module.exports = app;
