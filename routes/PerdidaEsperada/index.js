5a82253c9cad3f249c5604b318e17a8dea9e9b41
manumontes23 <manumontes23@gmail.com>
2020-10-19 15:53:22 -0500

    add CRUD endpoints

--- routes/PerdidaEsperada/index.js
+++ routes/PerdidaEsperada/index.js
@@ -1,12 +1,12 @@
 const controller = require("./perdidaEsperada.controller");
 const express = require("express");
 const router = express.Router();
-
-router.get("/perdidaEsperada", controller.obtenerPerdidaEsperada);
-router.post("/perdidaEsperada", controller.perdidaEsperada);
-router.put("/perdidaEsperada", controller.modificarPerdidaEsperada);
-router.delete("/perdidaEsperada", controller.eliminarPerdidaEsperada);
-router.get("/perdidaEsperada/recuperacion",controller.recuperacionPe);
+//rutas
+router.post("/perdidaEsperada/:id", controller.calcularPerdidaEsperada);
+router.get("/perdidaEsperada/:id", controller.obtenerValorPerdidaEsperada);
+router.put("/perdidaEsperada/:id", controller.calcularPerdidaEsperada);
+router.delete("/perdidaEsperada/:id", controller.eliminarPerdidaEsperada);
+router.get("/perdidaEsperada/:id/recuperacion",controller.recuperacionPerdidaEsperada)
 
 
 module.exports = router;
--- routes/PerdidaEsperada/perdidaEsperada.controller.js
+++ routes/PerdidaEsperada/perdidaEsperada.controller.js
@@ -6,25 +6,79 @@ const perdidaCollection = "perdidaEsperada";
 const NodeCache = require( "node-cache" );
 const myCache = new NodeCache();
 
-function perdidaEsperada(req, res) {
-    let body = req.body;
-    if(body.pd && body.ead && body.lgd){
-        let Pe = body.pd  * body.lgd * body.ead
-        myCache.set( 'PerdidaE',Pe, 1000 );
-        let fun = (dataBase) =>
-      dataBase.collection(collection).insertOne(data, (err, item) => {
+function calcularPerdidaEsperada(req, res) {
+  const { pd, lgd, ead } = req.body;
+  const { id } = req.params;
+
+  if (pd && lgd && ead && id) {
+    let fun = (dataBase) =>
+      dataBase
+        .collection(perdidaCollection)
+        .updateOne(
+          { id },
+          { $set: { perdidaEsperada: pd * lgd * ead } },
+          (err, item) => {
+            if (err) throw err;
+            if (item.result.n > 0) {
+              myCache.set( 'PerdidaE',pd * lgd * ead, 1000 )
+              res.status(200).send({
+                status: true,
+                data: {
+                  valor: pd * lgd * ead,
+                },
+                message: `Valor de la perdida esperada calculada`,
+              });
+            } else {
+              res.status(401).send({
+                status: false,
+                data: [],
+                message: `No se encontró la entidad`,
+              });
+            }
+          }
+        );
+
+    if (isThereAnyConnection(client)) {
+      const dataBase = client.db(DBName);
+      fun(dataBase);
+    } else {
+      client.connect((err) => {
         if (err) throw err;
-        if (item.result.n > 0) {
-          res.status(201).send({
+        const dataBase = client.db(DBName);
+        fun(dataBase);
+      });
+    }
+  } else {
+    res.status(400).send({
+      status: false,
+      data: [],
+      message: `No se pasaron los parametros necesarios`,
+    });
+  }
+}
+
+function obtenerValorPerdidaEsperada(req, res) {
+  const { id } = req.params;
+
+  if (id) {
+    let fun = (dataBase) =>
+      dataBase.collection(perdidaCollection).findOne({ id }, (err, item) => {
+        if (err) throw err;
+        if (item) {
+          myCache.set( 'PerdidaE',item.perdidaEsperada, 1000 )
+          res.status(200).send({
             status: true,
-            data,
-            message: `Elemento agregado exitosamente`,
+            data: {
+              entidad: item.id,
+              valorPerdidaEsperada: item.perdidaEsperada,
+            },
+            message: "Valor de la perdida esperada retornada",
           });
         } else {
           res.status(401).send({
             status: false,
             data: [],
-            message: `No se pudo crear el concepto, por favor intenta de nuevo`,
+            message: `No se encontró la entidad`,
           });
         }
       });
@@ -33,178 +87,84 @@ function perdidaEsperada(req, res) {
       const dataBase = client.db(DBName);
       fun(dataBase);
     } else {
-      client.connect((err) => { 
+      client.connect((err) => {
         if (err) throw err;
         const dataBase = client.db(DBName);
         fun(dataBase);
       });
     }
-    }else{
-        myCache.set( 'PerdidaE',0, 1000 );;
-        res.status(400).send({            
-            message:'Error al Calcular Perdida Esperada'
+  } else {
+    res.status(400).send({
+      status: false,
+      data: [],
+      message: `No se pasaron los parametros necesarios`,
+    });
+  }
+}
+function eliminarPerdidaEsperada(req, res) {
+  let { id } = req.params;
+
+  if (id) {
+    let fun = (dataBase) =>
+      dataBase
+        .collection(perdidaEsperada)
+        .deleteOne({ _id: ObjectID(id) }, (err, item) => {
+          if (err) throw err;
+          if (item.result.n > 0) {
+            myCache.set( 'PerdidaE',0, 1000 )
+            res.status(200).send({
+              status: true,
+              message: `Elemento eliminado`,
+            });
+          } else {
+            res.status(401).send({
+              status: false,
+              data: [],
+              message: `No se encontró el riesgo`,
+            });
+          }
         });
-    }  ;
-};
 
-function obtenerPerdidaEsperada(req, res) {
-    let { id } = req.params;
-    if (id) {
-      let fun = (dataBase) =>
-        dataBase
-          .collection(perdidaCollection)
-          .findOne({ _id: ObjectID(id) }, (err, item) => {
-            if (err) throw err;
-            if (item) {
-                myCache.set( 'PerdidaE',item.perdidaEsperada, 1000 )
-              res.status(201).send({
-                status: true,
-                data: item,
-                message: `Perdida Esperada de la empresa`,
-              });
-            } else {
-              res.status(401).send({
-                status: false,
-                data: [],
-                message: `No se encontro información sobre la perdida esperada`,
-              });
-            }
-          });
-          if (isThereAnyConnection(client)) {
-            const dataBase = client.db(DBName);
-            fun(dataBase);
-          } else {
-            client.connect((err) => {
-              if (err) throw err;
-              const dataBase = client.db(DBName);
-              fun(dataBase);
-            });
-          }
-        } else {
-          res.status(400).send({
-            status: false,
-            data: [],
-            message: `No se pasaron los parametros necesarios`,
-          });
-        }
-      };
+    if (isThereAnyConnection(client)) {
+      const dataBase = client.db(DBName);
+      fun(dataBase);
+    } else {
+      client.connect((err) => {
+        if (err) throw err;
+        const dataBase = client.db(DBName);
+        fun(dataBase);
+      });
+    }
+  } else {
+    res.status(400).send({
+      status: false,
+      data: [],
+      message: `No se pasaron los parametros necesarios`,
+    });
+  }
+}
 
-      function modificarPerdidaEsperada(req, res) {
-        const { pd, lgd, ead } = req.body;
-        const { id } = req.params;
-      
-        if (pd && lgd && ead && id) {
-            let Pe= pd * lgd * ead
-          myCache.set( 'PerdidaE',Pe, 1000 )
-          let fun = (dataBase) =>
-            dataBase
-              .collection(perdidaCollection)
-              .updateOne(
-                { id },
-                { $set: { perdidaEsperada: pd * lgd * ead } },
-                (err, item) => {
-                  if (err) throw err;
-                  if (item.result.n > 0) {
-                    res.status(200).send({
-                      status: true,
-                      data: {
-                        valor: pd * lgd * ead,
-                      },
-                      message: `Valor de la perdida esperada calculada`,
-                    });
-                  } else {
-                    res.status(401).send({
-                      status: false,
-                      data: [],
-                      message: `No se encontró la entidad`,
-                    });
-                  }
-                }
-              );
-      
-          if (isThereAnyConnection(client)) {
-            const dataBase = client.db(DBName);
-            fun(dataBase);
-          } else {
-            client.connect((err) => {
-              if (err) throw err;
-              const dataBase = client.db(DBName);
-              fun(dataBase);
-            });
-          }
-        } else {
-          res.status(400).send({
-            status: false,
-            data: [],
-            message: `No se pasaron los parametros necesarios`,
-          });
-        }
-      }
 
-      function eliminarPerdidaEsperada(req, res) {
-        let { id } = req.params;
-      
-        if (id) {
-          let fun = (dataBase) =>
-            dataBase
-              .collection(perdidaCollection)
-              .deleteOne({ _id: ObjectID(id) }, (err, item) => {
-                if (err) throw err;
-                if (item.result.n > 0) {
-                  res.status(200).send({
-                    status: true,
-                    message: `Elemento eliminado`,
-                  });
-                } else {
-                  res.status(401).send({
-                    status: false,
-                    data: [],
-                    message: `No se encontró la perdida esperada`,
-                  });
-                }
-              });
-      
-          if (isThereAnyConnection(client)) {
-            const dataBase = client.db(DBName);
-            fun(dataBase);
-          } else {
-            client.connect((err) => {
-              if (err) throw err;
-              const dataBase = client.db(DBName);
-              fun(dataBase);
-            });
-          }
-        } else {
-          res.status(400).send({
-            status: false,
-            data: [],
-            message: `No se pasaron los parametros necesarios`,
-          });
-        }
-      }      
-
-function recuperacionPe(req,res){    
-    let pe = myCache.get( 'PerdidaE');
-    console.log(pe)
-    if (pe!=null){
-        let recuPe = pe * 0.3;
-        res.status(200).send({
-            status: true,
-            data: recuPe,
-            message: `recuperación Calculada con exito`,
-          });
-    }else{
-        res.status(400).send({
-            message:'Error al Calcular la recuperacion en base a la Perdida Esperada'
+function recuperacionPerdidaEsperada(req,res){    
+  let pe = myCache.get( 'PerdidaE');
+  console.log(pe)
+  if (pe!=null){
+      let recuPe = pe * 0.3;
+      res.status(200).send({
+          status: true,
+          data: recuPe,
+          message: `recuperación Calculada con exito`,
         });
-    };
+  }else{
+      res.status(400).send({
+          message:'Error al Calcular la recuperacion en base a la Perdida Esperada'
+      });
+  };
 };
-
 
 module.exports={
-    perdidaEsperada,
-    recuperacionPe,
-    obtenerPerdidaEsperada,
-    modificarPerdidaEsperada,
-    eliminarPerdidaEsperada
+  calcularPerdidaEsperada,
+  obtenerValorPerdidaEsperada,
+  recuperacionPerdidaEsperada,
+  eliminarPerdidaEsperada
 };
