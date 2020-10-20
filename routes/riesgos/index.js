d9f6b434c9515d24758141756403e03e950b3afa
manumontes23 <manumontes23@gmail.com>
2020-10-19 23:44:43 -0500

    ...

--- routes/riesgos/index.js
+++ routes/riesgos/index.js
@@ -6,7 +6,7 @@ router.post("/riesgos", controller.crearRiesgo);
 router.get("/riesgos/:id", controller.obtenerRiesgo);
 router.put("/riesgos/:id", controller.modificarRiesgo);
 router.delete("/riesgos/:id", controller.eliminarRiesgo);
-router.post("/perdidaEsperada/:id", controller.calcularPerdidaEsperada);
-router.get("/perdidaEsperada/:id", controller.obtenerValorPerdidaEsperada);
+//# router.post("/perdidaEsperada/:id", controller.calcularPerdidaEsperada);
+//router.get("/perdidaEsperada/:id", controller.obtenerValorPerdidaEsperada);
 
 module.exports = router;
